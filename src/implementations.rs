469a1dd676d9cbc890f6af651aad9533b7d36588
Vitaly Bragilevsky <bravit111@gmail.com>
2022-07-11 13:48:42 +0300

    reserve enough memory with catalan number in advance

--- src/implementations.rs
+++ src/implementations.rs
@@ -16,3 +16,10 @@ pub fn find_implementation(impl_name: &str) -> Option<GenerateParenthesesFn> {
             impl_name == *name
     ).map(|(_, fun)| *fun)
 }
+
+pub fn catalan_number(n: usize) -> usize {
+    match n {
+        0 => 1,
+        _ => catalan_number(n - 1) * 2 * (2 * n - 1) / (n + 1)
+    }
+}
